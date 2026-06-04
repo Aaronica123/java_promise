60d3dfc92e38b1fbe6e7689f060ef7618a0caf28
Aaronica123 <k.aaronmutua@gmail.com>
2026-06-04 16:33:47 +0300

    register

--- display/system_admin/src/App.jsx
+++ display/system_admin/src/App.jsx
@@ -3,6 +3,7 @@ import Success_alert from './notifications/success.jsx';
 import Error from './notifications/error.jsx';
 import Logout from './notifications/logout.jsx';
 import Sidebar from './sidebar/sidebar.jsx';
+import Dash_1 from './dashboards/registry_dashboard.jsx';
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
     {/* <Success_alert /> */}
     {/* <Error/> */}
     {/* <Logout/> */}
-    <Sidebar/>
+    {/* <Sidebar/> */}
+    <Dash_1/>
     </>
   )
 }
---
+++ display/system_admin/src/dashboards/registry_dashboard.jsx
@@ -0,0 +1,89 @@
+
+import notification from "../images/others/notifications_logo.png";
+import profile from "../images/others/profile_logo.png";
+import "./registry.css";
+function Dash_1(){
+
+    return(
+    <>
+    <div className="dash_1">
+        <div className="dash_head">
+            <p>Dashboard</p>
+        </div>
+        <div className="dash_up">
+            <div className="dash_time">
+                <div className="dash_t1">
+                    <p>Time Clocked In</p>
+                </div>
+                <div className="dash_t2">
+                    <p>00:00:00</p>
+                </div>
+            </div>
+            <div className="dash_notifications">
+                <div className="dash_n1">
+                    <div className="dash_nt">
+                        <p>View All Notifications</p>
+                    </div>
+                    <div className="dash_image">
+                        <img src={notification}/>
+                    </div>
+                </div>
+                <div className="dash_n2">
+                    <div className="dash_nt">
+                        <p>View Profile</p>
+                    </div>
+                    <div className="dash_image">
+                        <img src={profile}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="dash_down">
+            <div className="dash_alerts">
+                <div className="dash_title">
+                    <p>Company Alerts</p>
+                </div>
+                <div className="dash_view">
+                    <div className="dash_data">
+                        <div className="data_body">
+                            <p>Company Alert</p>
+                        </div>
+                        <div className="data_body">
+                            <p>Company Alert</p>
+                        </div>
+                        <div className="data_body">
+                            <p>Company Alert</p>
+                        </div>
+                    </div>
+                    <div className="dash_click">
+                        <div className="dash_btn_alerts">
+                            <p>View Alerts</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="dash_tasks">
+                <div className="dash_title">
+                    <p>Pending Tasks</p>
+                </div>
+                <div className="dash_body">
+                    <p>Pending Task</p>
+                </div>
+                <div className="dash_body">
+                    <p>Pending Task</p>
+                </div>
+                <div className="dash_body">
+                    <p>Pending Task</p>
+                </div>
+                <div className="dash_btn_tasks">
+                    <p>View All Tasks</p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    </>)
+}
+export default Dash_1;
